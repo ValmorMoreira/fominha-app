f62863e719a38ba0371395c7ee866d5287b53d62
Andres <andres.porfirio@gmail.com>
2022-06-24 10:59:22 -0300

    fixed login - useEffect removed

--- src/services/sqlite/Users.jsx
+++ src/services/sqlite/Users.jsx
@@ -4,18 +4,17 @@ import user from "./SQLiteDatabse";
  * INICIALIZAÇÃO DA TABELA
  * - Executa sempre, mas só cria a tabela caso não exista (primeira execução)
  */
- user.transaction((tx) => {
-    //<<<<<<<<<<<<<<<<<<<<<<<< USE ISSO APENAS DURANTE OS TESTES!!! >>>>>>>>>>>>>>>>>>>>>>>
-    //tx.executeSql("DROP TABLE users;");
-    //<<<<<<<<<<<<<<<<<<<<<<<< USE ISSO APENAS DURANTE OS TESTES!!! >>>>>>>>>>>>>>>>>>>>>>>
-  
-    tx.executeSql(
-      "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(50), email VARCHAR(50), password VARCHAR(50));"
-    );
-  });
+user.transaction((tx) => {
+  //<<<<<<<<<<<<<<<<<<<<<<<< USE ISSO APENAS DURANTE OS TESTES!!! >>>>>>>>>>>>>>>>>>>>>>>
+  // tx.executeSql("DROP TABLE users;");
+  //<<<<<<<<<<<<<<<<<<<<<<<< USE ISSO APENAS DURANTE OS TESTES!!! >>>>>>>>>>>>>>>>>>>>>>>
 
+  tx.executeSql(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(50), email VARCHAR(50), password VARCHAR(50));"
+  );
+});
 
-  /**
+/**
  * CRIAÇÃO DE UM NOVO REGISTRO
  * - Recebe um objeto;
  * - Retorna uma Promise:
@@ -23,152 +22,151 @@ import user from "./SQLiteDatabse";
  *  - Pode retornar erro (reject) caso exista erro no SQL ou nos parâmetros.
  */
 const create = (obj) => {
-    return new Promise((resolve, reject) => {
-      user.transaction((tx) => {
-        //comando SQL modificável
-        tx.executeSql(
-          "INSERT INTO users (name, email, password) values (?, ?, ?);",
-          [obj.name, obj.email, obj.password],
-          //-----------------------
-          (_, { rowsAffected, insertId }) => {
-            if (rowsAffected > 0) resolve(insertId);
-            else reject("Error inserting obj: " + JSON.stringify(obj)); // insert falhou
-          },
-          (_, error) => reject(error) // erro interno em tx.executeSql
-        );
-      });
+  return new Promise((resolve, reject) => {
+    user.transaction((tx) => {
+      //comando SQL modificável
+      tx.executeSql(
+        "INSERT INTO users (name, email, password) values (?, ?, ?);",
+        [obj.name, obj.email, obj.password],
+        //-----------------------
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) resolve(insertId);
+          else reject("Error inserting obj: " + JSON.stringify(obj)); // insert falhou
+        },
+        (_, error) => reject(error) // erro interno em tx.executeSql
+      );
     });
-  };
-  
-  /**
-   * ATUALIZA UM REGISTRO JÁ EXISTENTE
-   * - Recebe o ID do registro e um OBJETO com valores atualizados;
-   * - Retorna uma Promise:
-   *  - O resultado da Promise é a quantidade de registros atualizados;
-   *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
-   */
-  const update = (id, obj) => {
-    return new Promise((resolve, reject) => {
-      user.transaction((tx) => {
-        //comando SQL modificável
-        tx.executeSql(
-          "UPDATE users SET name=?, email=? WHERE id=?;",
-          [obj.name, obj.email, id],
-          //-----------------------
-          (_, { rowsAffected }) => {
-            if (rowsAffected > 0) resolve(rowsAffected);
-            else reject("Error updating obj: id=" + id); // nenhum registro alterado
-          },
-          (_, error) => reject(error) // erro interno em tx.executeSql
-        );
-      });
+  });
+};
+
+/**
+ * ATUALIZA UM REGISTRO JÁ EXISTENTE
+ * - Recebe o ID do registro e um OBJETO com valores atualizados;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise é a quantidade de registros atualizados;
+ *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
+ */
+const update = (id, obj) => {
+  return new Promise((resolve, reject) => {
+    user.transaction((tx) => {
+      //comando SQL modificável
+      tx.executeSql(
+        "UPDATE users SET name=?, email=? WHERE id=?;",
+        [obj.name, obj.email, id],
+        //-----------------------
+        (_, { rowsAffected }) => {
+          if (rowsAffected > 0) resolve(rowsAffected);
+          else reject("Error updating obj: id=" + id); // nenhum registro alterado
+        },
+        (_, error) => reject(error) // erro interno em tx.executeSql
+      );
     });
-  };
-  
-  /**
-   * BUSCA UM REGISTRO POR MEIO DO ID
-   * - Recebe o ID do registro;
-   * - Retorna uma Promise:
-   *  - O resultado da Promise é o objeto (caso exista);
-   *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
-   */
-  const find = (id) => {
-    return new Promise((resolve, reject) => {
-      user.transaction((tx) => {
-        //comando SQL modificável
-        tx.executeSql(
-          "SELECT * FROM users WHERE id=?;",
-          [id],
-          //-----------------------
-          (_, { rows }) => {
-            if (rows.length > 0) resolve(rows._array[0]);
-            else reject("Obj not found: id=" + id); // nenhum registro encontrado
-          },
-          (_, error) => reject(error) // erro interno em tx.executeSql
-        );
-      });
+  });
+};
+
+/**
+ * BUSCA UM REGISTRO POR MEIO DO ID
+ * - Recebe o ID do registro;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise é o objeto (caso exista);
+ *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
+ */
+const find = (id) => {
+  return new Promise((resolve, reject) => {
+    user.transaction((tx) => {
+      //comando SQL modificável
+      tx.executeSql(
+        "SELECT * FROM users WHERE id=?;",
+        [id],
+        //-----------------------
+        (_, { rows }) => {
+          if (rows.length > 0) resolve(rows._array[0]);
+          else reject("Obj not found: id=" + id); // nenhum registro encontrado
+        },
+        (_, error) => reject(error) // erro interno em tx.executeSql
+      );
     });
-  };
-  
-  /**
-   * BUSCA UM REGISTRO POR MEIO DO EMAIL E PASSWORD VÁLIDOS
-   * - Recebe o nome do email e senha do usuário;
-   * - Retorna uma Promise:
-   *  - O resultado da Promise é um array com os objetos encontrados;
-   *  - Pode retornar erro (reject) caso o email e senha não sejam válidos ou então caso ocorra erro no SQL;
-   *  - Pode retornar um array vazio caso nenhum objeto seja encontrado.
-   */
-   const findLogin = (email) => {
-    return new Promise((resolve, reject) => {
-      user.transaction((tx) => {
-        //comando SQL modificável
-        tx.executeSql(
-          "SELECT * FROM users WHERE email = ?;",
-          [email],
-          //-----------------------
-          (_, { rows }) => {
-            if (rows.length > 0) resolve(rows._array[0]);
-            else reject("Obj not found: email "); // nenhum registro encontrado
-          },
-          (_, error) => reject(error) // erro interno em tx.executeSql
-        );
-      });
+  });
+};
+
+/**
+ * BUSCA UM REGISTRO POR MEIO DO EMAIL E PASSWORD VÁLIDOS
+ * - Recebe o nome do email e senha do usuário;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise é um array com os objetos encontrados;
+ *  - Pode retornar erro (reject) caso o email e senha não sejam válidos ou então caso ocorra erro no SQL;
+ *  - Pode retornar um array vazio caso nenhum objeto seja encontrado.
+ */
+const findLogin = (email) => {
+  return new Promise((resolve, reject) => {
+    user.transaction((tx) => {
+      //comando SQL modificável
+      tx.executeSql(
+        "SELECT * FROM users WHERE email = ?;",
+        [email],
+        //-----------------------
+        (_, { rows }) => {
+          if (rows.length > 0) resolve(rows._array[0]);
+          else reject("Obj not found: " + email); // nenhum registro encontrado
+        },
+        (_, error) => reject(error) // erro interno em tx.executeSql
+      );
     });
-  };
-  
-  /**
-   * BUSCA TODOS OS REGISTROS DE UMA DETERMINADA TABELA
-   * - Não recebe parâmetros;
-   * - Retorna uma Promise:
-   *  - O resultado da Promise é uma lista (Array) de objetos;
-   *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL;
-   *  - Pode retornar um array vazio caso não existam registros.
-   */
-  const all = () => {
-    return new Promise((resolve, reject) => {
-      user.transaction((tx) => {
-        //comando SQL modificável
-        tx.executeSql(
-          "SELECT * FROM users;",
-          [],
-          //-----------------------
-          (_, { rows }) => resolve(rows._array),
-          (_, error) => reject(error) // erro interno em tx.executeSql
-        );
-      });
+  });
+};
+
+/**
+ * BUSCA TODOS OS REGISTROS DE UMA DETERMINADA TABELA
+ * - Não recebe parâmetros;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise é uma lista (Array) de objetos;
+ *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL;
+ *  - Pode retornar um array vazio caso não existam registros.
+ */
+const all = () => {
+  return new Promise((resolve, reject) => {
+    user.transaction((tx) => {
+      //comando SQL modificável
+      tx.executeSql(
+        "SELECT * FROM users;",
+        [],
+        //-----------------------
+        (_, { rows }) => resolve(rows._array),
+        (_, error) => reject(error) // erro interno em tx.executeSql
+      );
     });
-  };
-  
-  /**
-   * REMOVE UM REGISTRO POR MEIO DO ID
-   * - Recebe o ID do registro;
-   * - Retorna uma Promise:
-   *  - O resultado da Promise a quantidade de registros removidos (zero indica que nada foi removido);
-   *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
-   */
-  const remove = (id) => {
-    return new Promise((resolve, reject) => {
-      user.transaction((tx) => {
-        //comando SQL modificável
-        tx.executeSql(
-          "DELETE FROM users WHERE id=?;",
-          [id],
-          //-----------------------
-          (_, { rowsAffected }) => {
-            resolve(rowsAffected);
-          },
-          (_, error) => reject(error) // erro interno em tx.executeSql
-        );
-      });
+  });
+};
+
+/**
+ * REMOVE UM REGISTRO POR MEIO DO ID
+ * - Recebe o ID do registro;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise a quantidade de registros removidos (zero indica que nada foi removido);
+ *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
+ */
+const remove = (id) => {
+  return new Promise((resolve, reject) => {
+    user.transaction((tx) => {
+      //comando SQL modificável
+      tx.executeSql(
+        "DELETE FROM users WHERE id=?;",
+        [id],
+        //-----------------------
+        (_, { rowsAffected }) => {
+          resolve(rowsAffected);
+        },
+        (_, error) => reject(error) // erro interno em tx.executeSql
+      );
     });
-  };
-  
-  export default {
-    create,
-    update,
-    find,
-    findLogin,
-    all,
-    remove,
-  };
-  
+  });
+};
+
+export default {
+  create,
+  update,
+  find,
+  findLogin,
+  all,
+  remove,
+};
